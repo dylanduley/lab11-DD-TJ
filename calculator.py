d8ab2689f1881c2e03aaae06fe1e16466c19fa50
Koshie <tydy.jack@gmail.com>
2026-08-01 11:30:30 -0400

    fix merged calculator functions

--- calculator.py
+++ calculator.py
@@ -1,19 +1,40 @@
-"""
-calculator.py
-- Defines functions used to create a simple calculator
+# https://github.com/dylanduley/lab11-DD-TJ
+# Partner 1: Dylan Duley
+# Partner 2: Ty Jackson
 
-One function per operation, in order.
-"""
 import math
+
 
 def add(a, b):
     return a + b
 
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def logarithm(a, b):
+    if a <= 0 or a == 1 or b <= 0:
+        raise ValueError
+
+    return math.log(b, a)
+
+
+def exponent(a, b):
+    return a ** b
+
+
 def sub(a, b):
     return a - b
 
+
 def mul(a, b):
     return a * b
+
 
 def div(a, b):
     if a == 0:
@@ -21,11 +42,13 @@ def div(a, b):
 
     return b / a
 
+
 def log(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError
 
     return math.log(b, a)
+
 
 def exp(a, b):
     return a ** b
